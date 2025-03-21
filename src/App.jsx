6dcb409f264d8cd0869c09e258ff7becaacd0b69
Mike Jackson <mike@airtightdesign.com>
2025-03-21 17:49:41 -0400

    Build product row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from './components/Navigation'
+import ProductRow from './components/ProductRow'
 import './App.css'
 
 function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
-  const excludedCategories = ["electronics", "string", "miscellaneous"];
 
   useEffect(() => {
     axios.get('https://fakestoreapi.com/products')
@@ -18,6 +18,8 @@ function App() {
     });
   }, []);
 
+  
+
   const addToCart = (product) => {
     setCart((prevCart) => [...prevCart, product]);
     console.log(`${product.title} added to bag!`);
@@ -27,29 +29,21 @@ function App() {
   return (
     <>
     <Navbar cart={cart} />
-      <div id="content" className="px-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-        {products.map((product) => (
-          <>
-            <div key={product.id} className="product relative relative mb-8">
-              <div className="image-parent mb-4 p-8 border-solid border-gray-400 border-1 p-1 ">
-                <img className="rounded mx-auto" 
-                  src={product.image} 
-                  alt={product.name} 
-                  />
-              </div>
-              <div className="flex gap-1 icon-parent flex-col">
-                <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-primary text-white rounded hover:brightness-90 transition duration-300" onClick={() => addToCart(product)}><i className="fa-solid fa-plus"></i></button>
-                <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-tangerine text-white rounded hover:brightness-90 transition duration-300"><i className="fa-solid fa-eye"></i></button>
-              </div>
-              <p className="capitalize text-sm">{product.category}</p>
-              <h3 className="my-1 text-lg/5">{product.title}</h3>
-              {/* <p>{product.description}</p> */}
-              <p>${product.price}</p>
-              
-            </div>
-          </>
-        ))}
-      </div>
+     <ProductRow 
+      category="men's clothing"
+      products={products.filter(p => p.category === "men's clothing").slice(0, 4)}
+      addToCart={addToCart}
+     />
+     <ProductRow 
+      category="womens's clothing"
+      products={products.filter(p => p.category === "women's clothing").slice(0, 4)}
+      addToCart={addToCart}
+     />
+     <ProductRow 
+      category="electronics"
+      products={products.filter(p => p.category === "electronics").slice(0, 4)}
+      addToCart={addToCart}
+     />
     </>
   )
 }
---
+++ src/components/ProductRow.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+const ProductRow = ({ category, products, addToCart }) => {
+
+    return(
+      <div id="{category}" className="container px-4 mb-4">
+        <div className="flex justify-between items-center">
+            <h2 className="text-2xl capitalize mb-2">{category}</h2>
+            <a className="right-caret group" href="">View All <i class="transition-transform duration-300 group-hover:translate-x-1  fa-solid fa-chevron-right"></i></a>
+        </div>
+        <div className="gap-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        {products.map((product) => (
+            <div key={product.id} className="product relative relative mb-8">
+              <div className="image-parent mb-4 p-8 border-solid border-gray-400 border-1 p-1 ">
+                <img className="rounded mx-auto" 
+                  src={product.image} 
+                  alt={product.name} 
+                  />
+              </div>
+              <div className="flex gap-1 icon-parent flex-col">
+                <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-primary text-white rounded hover:brightness-90 transition duration-300" onClick={() => addToCart(product)}><i className="fa-solid fa-plus"></i></button>
+                <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-tangerine text-white rounded hover:brightness-90 transition duration-300"><i className="fa-solid fa-eye"></i></button>
+              </div>
+              <h3 className="my-1 text-lg/5">{product.title}</h3>
+              <p className="capitalize text-sm">{product.category}</p>
+              {/* <p>{product.description}</p> */}
+              <p>${product.price}</p>
+              
+            </div>
+        ))}
+        </div>
+      </div>
+    )
+}
+
+export default ProductRow;
