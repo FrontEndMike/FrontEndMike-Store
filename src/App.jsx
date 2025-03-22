9acde9ab4679a71d652aec4afa886b0c6b581bfd
Mike Jackson <mike@airtightdesign.com>
2025-03-22 18:36:21 -0400

    global style tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from './components/Navigation'
+import Hero from './components/Hero'
+import SideCart from './components/SideCart'
 import ProductRow from './components/ProductRow'
 import './App.css'
 
@@ -18,32 +20,72 @@ function App() {
     });
   }, []);
 
-  
-
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
-    console.log(`${product.title} added to bag!`);
-    console.log(cart);
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      //Check if the id of the product already exists in the array
+      if (existingItem) {
+        return prevCart.map((item) =>  // If it exists loop through the current array
+          item.id === product.id //If id match
+            ? { ...item, quantity: item.quantity + 1 }  //Create a copy of that item with the quantity property increased by 1
+            : item //Else return item unchanged
+        );
+      } else {
+        // If it doesnt exist, add the product to the cart with the quantity property of 1
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+    console.log(`${product.title} added to cart`);
   };
+
+  const removeFromCart = (product) => {
+    setCart((prevCart) => {
+      return prevCart
+        .map(item =>
+          item.id === product.id //Find the item in the with the id that matches the current product
+            ? { ...item, quantity: item.quantity - 1 } //Create a copy of that array with the quantity reduced by 1
+            : item
+        )
+        .filter(item => item.quantity > 0); // Remove completely if 0
+    });
+        console.log(`${product.title} removed from cart`);
+  };
+
+const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+const totalPrice = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+
 
   return (
     <>
-    <Navbar cart={cart} />
-     <ProductRow 
-      category="men's clothing"
-      products={products.filter(p => p.category === "men's clothing").slice(0, 4)}
-      addToCart={addToCart}
-     />
-     <ProductRow 
-      category="womens's clothing"
-      products={products.filter(p => p.category === "women's clothing").slice(0, 4)}
-      addToCart={addToCart}
-     />
-     <ProductRow 
-      category="electronics"
-      products={products.filter(p => p.category === "electronics").slice(0, 4)}
-      addToCart={addToCart}
-     />
+    <Navbar totalItems={totalItems} />
+    <Hero />
+    <SideCart 
+      cart={cart} 
+      totalItems={totalItems} 
+      addToCart={addToCart} 
+      totalPrice={totalPrice}
+      removeFromCart={removeFromCart} 
+    />
+    <div className="container max-w-[1280px] mx-auto my-8">
+    <div id="content">
+      <ProductRow 
+        category="men's clothing"
+        products={products.filter(p => p.category === "men's clothing").slice(0, 4)}
+        addToCart={addToCart}
+      />
+      <ProductRow 
+        category="womens's clothing"
+        products={products.filter(p => p.category === "women's clothing").slice(0, 4)}
+        addToCart={addToCart}
+      />
+      <ProductRow 
+        category="electronics"
+        products={products.filter(p => p.category === "electronics").slice(0, 4)}
+        addToCart={addToCart}
+      />
+    </div>
+    </div>
     </>
   )
 }
--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -5,14 +5,14 @@ const ProductRow = ({ category, products, addToCart }) => {
 
     return(
       <div id="{category}" className="container px-4 mb-4">
-        <div className="flex justify-between items-center">
-            <h2 className="text-2xl capitalize mb-2">{category}</h2>
+        <div className="flex justify-between items-center mb-2">
+            <h2 className="text-2xl capitalize  text-secondary">{category}</h2>
             <a className="right-caret group" href="">View All <i className="transition-transform duration-300 group-hover:translate-x-1  fa-solid fa-chevron-right"></i></a>
         </div>
         <div className="gap-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
             <div key={product.id} className="product relative relative mb-8">
-              <div className="image-parent mb-4 p-8 border-solid border-gray-400 border-1 p-1 ">
+              <div className="image-parent mb-4 p-8 rounded-lg border-solid border-primary  p-1 ">
                 <img className="rounded mx-auto" 
                   src={product.image} 
                   alt={product.name} 
@@ -22,8 +22,8 @@ const ProductRow = ({ category, products, addToCart }) => {
                 <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-primary text-white rounded hover:brightness-90 transition duration-300" onClick={() => addToCart(product)}><i className="fa-solid fa-plus"></i></button>
                 <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-tangerine text-white rounded hover:brightness-90 transition duration-300"><i className="fa-solid fa-eye"></i></button>
               </div>
-              <h3 className="my-1 text-lg/5">{product.title}</h3>
-              <p className="capitalize text-sm">{product.category}</p>
+              <h3 className="my-1 text-lg/5 text-secondary">{product.title}</h3>
+              <p className="capitalize text-sm mb-1 ">{product.category}</p>
               {/* <p>{product.description}</p> */}
               <p>${product.price.toFixed(2)}</p>
               
