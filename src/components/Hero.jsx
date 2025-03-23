d1231272c4324db4b5eed3c933799042fa27a30f
Mike Jackson <mike@airtightdesign.com>
2025-03-23 00:55:32 -0400

    refactor hero component to be reusable

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 
-const Hero = ({ }) => {
+const Hero = ({ heroImage, headline, subheadline, introText, buttons }) => {
     return(
-        <div className="max-h-[600px] hero relative h-screen w-full flex items-center justify-start text-left bg-cover bg-center" >
+        <div className="hero" style={{ 
+            backgroundImage: `url(${heroImage})`
+        }}>
         <div className="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75"></div>
         
             <div className="max-w-[1280px] mx-auto w-full z-2">
                 <div className="px-4 text-left ">
                     <h1 className="text-4xl leading-10 font-extrabold sm:text-5xl text-white sm:leading-none md:text-6xl">
-                        FrontEndMike 
-                        <span className="text-tangerine block">React Store</span>
+                        {headline} 
+                        {subheadline && (
+                            <span className="text-tangerine block">{subheadline}</span>
+                        )}
                     </h1>
-                    <p className="mt-3 text-white sm:mt-5 sm:max-w-xl md:mt-5 text-lg font-light">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                    </p>
-                    <div className="mt-5 sm:mt-8 flex-wrap flex gap-4 justify-start">
-                        <a href="#content" className="w-auto cursor-pointer mt-2 px-4 py-2 bg-primary text-white rounded hover:brightness-90 transition duration-300">
-                            Show Now
-                        </a>
+                    {introText && (
+                        <p className="mt-3 text-white sm:mt-5 sm:max-w-xl md:mt-5 text-lg font-light">
+                            {introText}
+                        </p>
+                    )}
+                    {buttons && (
+                        <div className="mt-5 sm:mt-8 flex-wrap flex gap-4 justify-start">
+                            <a href="#content" className="w-auto cursor-pointer mt-2 px-4 py-2 bg-primary text-white rounded hover:brightness-90 transition duration-300">
+                                Show Now
+                            </a>
 
-                        <a href="#" className="w-auto cursor-pointer mt-2 px-4 py-2 bg-tangerine text-white rounded hover:brightness-90 transition duration-300">
-                            Live demo
-                        </a>
-                    </div>
+                            <a href="#" className="w-auto cursor-pointer mt-2 px-4 py-2 bg-tangerine text-white rounded hover:brightness-90 transition duration-300">
+                                Live demo
+                            </a>
+                        </div>
+                    )}
                 </div>
             </div>
         
--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,28 +1,35 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 
 const ProductRow = ({ category, products, addToCart }) => {
 
     return(
-      <div id="{category}" className="container px-4 mb-4">
+      <div id={category} className="mb-4">
         <div className="flex justify-between items-center mb-2">
-            <h2 className="text-2xl capitalize  text-secondary">{category}</h2>
-            <a className="right-caret group" href="">View All <i className="transition-transform duration-300 group-hover:translate-x-1  fa-solid fa-chevron-right"></i></a>
+            <h2 className="text-2xl capitalize  text-secondary">Shop Now</h2>
         </div>
         <div className="gap-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
             <div key={product.id} className="product relative relative mb-8">
-              <div className="image-parent mb-4 p-8 rounded-lg border-solid border-primary  p-1 ">
-                <img className="rounded mx-auto" 
-                  src={product.image} 
-                  alt={product.name} 
-                  />
+              <div className="isolate relative image-parent p-4 rounded-lg border-solid border-primary justify-center ">
+                <Link className="text-center" to={`/product/${product.id}`}>
+                  <img className="rounded mx-auto" 
+                    src={product.image} 
+                    alt={product.name} 
+                    />
+                </Link>
               </div>
               <div className="flex gap-1 icon-parent flex-col">
                 <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-primary text-white rounded hover:brightness-90 transition duration-300" onClick={() => addToCart(product)}><i className="fa-solid fa-plus"></i></button>
-                <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-tangerine text-white rounded hover:brightness-90 transition duration-300"><i className="fa-solid fa-eye"></i></button>
+                <Link to={`/product/${product.id}`}>
+                  <button className="w-auto cursor-pointer mt-2 px-4 py-2 bg-tangerine text-white rounded hover:brightness-90 transition duration-300">
+                    <i className="fa-solid fa-eye"><span className="sr-only">View Item</span>
+                    </i></button>
+                </Link>
               </div>
-              <h3 className="my-1 text-lg/5 text-secondary">{product.title}</h3>
+              <Link to={`/product/${product.id}`}>
+                <h3 className="my-1 text-lg/5 text-secondary">{product.title}</h3>
+              </Link>
               <p className="capitalize text-sm mb-1 ">{product.category}</p>
               {/* <p>{product.description}</p> */}
               <p>${product.price.toFixed(2)}</p>
