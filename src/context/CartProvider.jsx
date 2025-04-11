1080e3e8598b0cabcf28c619256424c100607f05
Mike Jackson <michael.lenard.jackson@gmail.com>
2025-04-11 17:42:17 -0400

    seperate context and provider concerns, create useCarthook

---
+++ src/context/CartProvider.jsx
@@ -0,0 +1,39 @@
+import React, { useMemo, useState } from 'react';
+import { CartStateContext, CartActionsContext } from './CartContext';
+import useCart from '../hooks/useCart'; // or wherever your logic is
+
+const CartProvider = ({ children }) => {
+  const {
+    cart,
+    totalItems,
+    totalPrice,
+    addToCart,
+    removeFromCart
+  } = useCart();
+
+  const [isCartActive, setIsCartActive] = useState(false);
+  const toggleCart = () => setIsCartActive(prev => !prev);
+
+  const stateValue = useMemo(() => ({
+    cart,
+    totalItems,
+    totalPrice,
+    isCartActive,
+    toggleCart,
+  }), [cart, totalItems, totalPrice, isCartActive]);
+
+  const actionsValue = useMemo(() => ({
+    addToCart,
+    removeFromCart,
+  }), [addToCart, removeFromCart]);
+
+  return (
+    <CartStateContext.Provider value={stateValue}>
+      <CartActionsContext.Provider value={actionsValue}>
+        {children}
+      </CartActionsContext.Provider>
+    </CartStateContext.Provider>
+  );
+};
+
+export default CartProvider;
